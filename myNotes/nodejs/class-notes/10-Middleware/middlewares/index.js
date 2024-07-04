935ba00a3169a05cdedf462cfee5fb6b1245d333
sonerylmz26 <snrylmz3126@gmail.com>
2024-07-04 14:04:22 -0500

    middleware exports

--- myNotes/nodejs/class-notes/10-Middleware/middlewares/index.js
+++ myNotes/nodejs/class-notes/10-Middleware/middlewares/index.js
@@ -1,6 +1,7 @@
-
-
-
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - MIDDLEWARES
+------------------------------------------------------- *
 
 const middleware1 = (req, res, next) => {
     console.log('middleware-1 çalıştı.')
@@ -18,4 +19,28 @@ const middleware3 = (req, res, next) => {
     next()
 }
 
-module.exports = [middleware1, middleware2, middleware3] //* {iddleware1, middleware2, middleware3}
+// module.exports = [ middleware1, middleware2, middleware3 ]
+module.exports = { middleware1, middleware2, middleware3 }
+
+/* ------------------------------------------------------- */
+
+module.exports = {
+
+    middleware1: (req, res, next) => {
+        console.log('middleware-1 çalıştı.')
+        next()
+    },
+
+    middleware2: (req, res, next) => {
+        console.log('middleware-2 çalıştı.')
+        next()
+    },
+
+    middleware3: (req, res, next) => {
+        console.log('middleware-3 çalıştı.')
+        next()
+    },
+
+}
+
+/* ------------------------------------------------------- */
