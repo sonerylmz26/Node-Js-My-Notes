e3e4344c53354193391b09f71c606a1ded29f6f5
sonerylmz26 <snrylmz3126@gmail.com>
2024-07-23 19:02:26 -0500

    up

--- myNotes/nodejs/class-notes/17-BlogAPI_/src/models/userModel.js
+++ myNotes/nodejs/class-notes/17-BlogAPI_/src/models/userModel.js
@@ -4,8 +4,24 @@ const mongoose = require('mongoose');
 // const { Schema} = require('mongoose');
 
 
-const passwordEncrypt = require('../helpers/passwordEncrypt')
 
+/* ------------------------------------------------------- */
+// Password Encrypt (PBKDF2 Method):
+// https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+const crypto = require('node:crypto')
+
+/* ------------------------------------------------------- */
+const keyCode = process.env.SECRET_KEY
+const loopCount = 10_000
+const charCount = 32
+const encType = 'sha512'
+
+
+const passwordEncrypt = function (password) {
+    return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+    
+}
+console.log(passwordEncrypt('12313'))
 /* ------------------------------------------------------- */
 const userShema = new mongoose.Schema({
 email:{
@@ -25,7 +41,7 @@ password:{
     // set: (password) => passwordEncrypt(password) // Veri kaydederken, return edilen data kaydedilir.
     //    set: passwordEncrypt,
     set: (password) => (password.length >= 8 ?  passwordEncrypt(password) : 'wrong'),
-        validate: (password) => (password != 'wrong') // Güncelleme yaparken default olarak validate çalışmaz. // { runValidators: true }
+        validate: (password) => (password != 'wrong') // Güncelleme yaparken default olarak validate çalışmaz. // { runValidators: true }  
         // Set methodu validate methodundan önce çalışır. Dolayısı ile validate datası her zaman aynı formattadır.
         // set: (password) => {
         //     if (password.length >= 8) {
