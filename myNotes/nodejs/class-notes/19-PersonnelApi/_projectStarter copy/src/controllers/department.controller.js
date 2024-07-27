0704afe7eca7ddaec50735a911012936ebff36d9
sonerylmz26 <snrylmz3126@gmail.com>
2024-07-27 06:19:45 -0500

    model created

--- myNotes/nodejs/class-notes/19-PersonnelApi/_projectStarter copy/src/controllers/department.controller.js
+++ myNotes/nodejs/class-notes/19-PersonnelApi/_projectStarter copy/src/controllers/department.controller.js
@@ -2,3 +2,57 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Department = require('../models/department.model')
+
+
+module.exports = {
+list: async(req,res) => {
+  
+    const data = await res.getModelDetails(Department)
+
+    res.status(200).send(
+        {
+            error:false,
+            data,
+            detail: await res.getModelListDetails(Department)
+        }
+    )
+},
+create: async (req, res) => {
+    const data = await Department.create(req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    }); 
+  },
+
+  read: async (req, res) => {
+    const data = await Department.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+update: async(req,res) => {
+  
+
+    res.status(200).send(
+        {
+            error:false,
+        }
+    )
+},
+delete: async(req,res) => {
+  
+
+    res.status(200).send(
+        {
+            error:false,
+        }
+    )
+}
+
+
+}
--- myNotes/nodejs/class-notes/19-PersonnelApi/_projectStarter copy/src/models/department.model.js
+++ myNotes/nodejs/class-notes/19-PersonnelApi/_projectStarter copy/src/models/department.model.js
@@ -4,3 +4,21 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const DepartmentSchema = new mongoose.Schema(  
+    {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },{
+    collation:'departments',
+    timestamps:true
+})
+
+module.exports = mongoose.model('Department',DepartmentSchema)
+// module.exports ={
+//     Department : mongoose.model('Department', DepartmentSchema),
+// }
